1118e689801d89b456255aff34d24b078ab8e67b
Stewart Anderson <stander@extendhealth.com>
2020-02-06 15:29:57 -0700

    Rough draft of validation is working.

--- src/Leaf.test.tsx
+++ src/Leaf.test.tsx
@@ -4,120 +4,139 @@ import { Leaf, useValidationFor } from './Leaf'
 import { TextInput } from './TextInput'
 import { get, set } from './domain'
 
-describe("Component API", () => {
-    it("can read & edit model nodes nested inside complex object models and arrays", () => {
-        const Wrapper = () => {
-            const [model, setModel] = useState({
-                lists: {
-                    emails: [
-                        { email: "stewie1570@gmail.com" },
-                        { email: "something_at_something.com" }
-                    ]
-                }
-            });
+test("can read & edit model nodes nested inside complex object models and arrays", () => {
+    const Wrapper = () => {
+        const [model, setModel] = useState({
+            lists: {
+                emails: [
+                    { email: "stewie1570@gmail.com" },
+                    { email: "something_at_something.com" }
+                ]
+            }
+        });
 
-            return <Leaf model={model} onChange={setModel} location="lists.emails">
-                {(emailNodes: Array<any>, setEmailNodes) => emailNodes.map((emailNode, index) => <Leaf
-                    key={index}
-                    model={emailNode}
-                    onChange={update => setEmailNodes(emailNodes.map((orig, i) => i === index ? update : orig))}
-                    location={`email`}>
-                    {(email: string, onChange) => <TextInput
-                        data-testid={`email${index}`}
-                        value={email}
-                        onChange={onChange} />}
-                </Leaf>)}
-            </Leaf>
-        }
+        return <Leaf model={model} onChange={setModel} location="lists.emails">
+            {(emailNodes: Array<any>, setEmailNodes) => emailNodes.map((emailNode, index) => <Leaf
+                key={index}
+                model={emailNode}
+                onChange={update => setEmailNodes(emailNodes.map((orig, i) => i === index ? update : orig))}
+                location={`email`}>
+                {(email: string, onChange) => <TextInput
+                    data-testid={`email${index}`}
+                    value={email}
+                    onChange={onChange} />}
+            </Leaf>)}
+        </Leaf>
+    }
 
-        const { getByTestId } = render(<Wrapper />);
+    const { getByTestId } = render(<Wrapper />);
 
-        expect((getByTestId("email0") as HTMLInputElement).value).toBe("stewie1570@gmail.com");
-        expect((getByTestId("email1") as HTMLInputElement).value).toBe("something_at_something.com");
+    expect((getByTestId("email0") as HTMLInputElement).value).toBe("stewie1570@gmail.com");
+    expect((getByTestId("email1") as HTMLInputElement).value).toBe("something_at_something.com");
 
-        fireEvent.change(getByTestId("email1"), { target: { value: "stewie1570@hotmail.com" } });
+    fireEvent.change(getByTestId("email1"), { target: { value: "stewie1570@hotmail.com" } });
 
-        expect((getByTestId("email0") as HTMLInputElement).value).toBe("stewie1570@gmail.com");
-        expect((getByTestId("email1") as HTMLInputElement).value).toBe("stewie1570@hotmail.com");
-    });
+    expect((getByTestId("email0") as HTMLInputElement).value).toBe("stewie1570@gmail.com");
+    expect((getByTestId("email1") as HTMLInputElement).value).toBe("stewie1570@hotmail.com");
 });
 
-describe("Get/Set model API", () => {
-    it("can read & edit model nodes inside complex object models and arrays", () => {
-        const Wrapper = () => {
-            const [model, setModel] = useState({
-                lists: {
-                    emails: [
-                        { email: "stewie1570@gmail.com" },
-                        { email: "something_at_something.com" }
-                    ]
-                }
-            });
-            return get<any>("lists.emails")
-                .from(model)
-                .map(({ email }: any, index: number) =>
-                    <TextInput
-                        key={index}
-                        data-testid={`email${index}`}
-                        value={email}
-                        onChange={(emailAddress: string) => setModel(set(`lists.emails.${index}.email`).to(emailAddress).in(model))} />);
-        }
+test("validate model node and show errors on blur", () => {
+    const Wrapper = () => {
+        const [model, setModel] = useState({ contact: { email: "stewie1570@gmail.com" } });
+        const validationModel = useValidationFor(model);
 
-        const { getByTestId } = render(<Wrapper />);
+        const isRequired = (value: string) => (!value || value.trim() === "") && ["Value is required"];
 
-        expect((getByTestId("email0") as HTMLInputElement).value).toBe("stewie1570@gmail.com");
-        expect((getByTestId("email1") as HTMLInputElement).value).toBe("something_at_something.com");
+        return <Leaf model={model} onChange={setModel} location="contact.email" validationModel={validationModel} validators={[isRequired]}>
+            {(email: string, onChange, onBlur, errors) => <>
+                <TextInput value={email} onChange={onChange} onBlur={onBlur} data-testid="email" />
+                {errors.length > 0 && <ul>
+                    {errors.map((error, index) => <li data-testid="error" key={index}>{error}</li>)}
+                </ul>}
+            </>}
+        </Leaf>
+    }
 
-        fireEvent.change(getByTestId("email1"), { target: { value: "stewie1570@hotmail.com" } });
+    const { getByTestId, queryAllByTestId, getAllByTestId } = render(<Wrapper />);
 
-        expect((getByTestId("email0") as HTMLInputElement).value).toBe("stewie1570@gmail.com");
-        expect((getByTestId("email1") as HTMLInputElement).value).toBe("stewie1570@hotmail.com");
-    });
+    expect(queryAllByTestId("error").map(node => node.textContent)).toEqual([]);
+    fireEvent.change(getByTestId("email"), { target: { value: "" } });
+    expect(queryAllByTestId("error").map(node => node.textContent)).toEqual([]);
+    fireEvent.blur(getByTestId("email"));
+    expect(getAllByTestId("error").map(node => node.textContent)).toEqual(["Value is required"]);
+});
 
-    it("validates model nodes inside complex object models and arrays", () => {
-        const Wrapper = () => {
-            const [model, setModel] = useState({
-                lists: {
-                    emails: [
-                        { email: "stewie1570@gmail.com" },
-                        { email: "something_at_something.com" }
-                    ]
-                }
-            });
-            const { validate, validationResultsFor } = useValidationFor(model);
+test("validate model immediately show errors", () => {
+    const Wrapper = () => {
+        const [model, setModel] = useState({ contact: { email: "" } });
+        const validationModel = useValidationFor(model);
 
-            const isRequired = (value: any) => value || ["Value is required"];
+        const isRequired = (value: string) => (!value || value.trim() === "") && ["Value is required"];
 
-            return get<any>("lists.emails")
-                .from(model)
-                .map(({ email }: any, index: number) =>
-                    <div key={index}>
-                        <TextInput
-                            data-testid={`email${index}`}
-                            value={email}
-                            onBlur={() => validate(`lists.emails.${index}.email`).conformsTo(isRequired)}
-                            onChange={(emailAddress: string) => setModel(set(`lists.emails.${index}.email`).to(emailAddress).in(model))} />
-                        <ul>
-                            {validationResultsFor(`lists.emails.${index}.email`)
-                                .map((validationError: any, errorIndex: number) =>
-                                    <li data-testid={`email${index}error`} key={errorIndex}>
-                                        {validationError}
-                                    </li>)}
-                        </ul>
-                    </div>)
-        }
+        return <Leaf
+            model={model}
+            onChange={setModel}
+            location="contact.email"
+            validationModel={validationModel}
+            validators={[isRequired]}
+            showErrors={true}>
+            {(email: string, onChange, onBlur, errors) => <>
+                <TextInput value={email} onChange={onChange} onBlur={onBlur} />
+                {errors.length > 0 && <ul>
+                    {errors.map((error, index) => <li data-testid="error" key={index}>{error}</li>)}
+                </ul>}
+            </>}
+        </Leaf>
+    }
 
-        const { queryAllByTestId, getAllByTestId, getByTestId } = render(<Wrapper />);
+    const { getAllByTestId } = render(<Wrapper />);
 
-        expect(queryAllByTestId("email1error")).toEqual([]);
+    expect(getAllByTestId("error").map(node => node.textContent)).toEqual(["Value is required"]);
+});
 
-        fireEvent.change(getByTestId("email0"), { target: { value: "" } });
-        fireEvent.blur(getByTestId("email0"));
+test("validate multiple model nodes", () => {
+    const Wrapper = () => {
+        const [model, setModel] = useState({ contact: { firstName: "", lastName: "" } });
+        const validationModel = useValidationFor(model);
 
-        fireEvent.change(getByTestId("email1"), { target: { value: "" } });
-        fireEvent.blur(getByTestId("email1"));
+        const isRequired = (value: string) => (!value || value.trim() === "") && ["Value is required"];
 
-        expect(getAllByTestId("email0error").map(element => element.textContent)).toEqual(["Value is required"]);
-        expect(getAllByTestId("email1error").map(element => element.textContent)).toEqual(["Value is required"]);
-    });
-})
+        return <>
+            <Leaf
+                model={model}
+                onChange={setModel}
+                location="contact.firstName"
+                validationModel={validationModel}
+                validators={[isRequired]}
+                showErrors={true}>
+                {(email: string, onChange, onBlur, errors) => <>
+                    <TextInput value={email} onChange={onChange} onBlur={onBlur} />
+                    {errors.length > 0 && <ul>
+                        {errors.map((error, index) => <li data-testid="error" key={index}>{error}</li>)}
+                    </ul>}
+                </>}
+            </Leaf>
+            <Leaf
+                model={model}
+                onChange={setModel}
+                location="contact.lastName"
+                validationModel={validationModel}
+                validators={[isRequired]}
+                showErrors={true}>
+                {(email: string, onChange, onBlur, errors) => <>
+                    <TextInput value={email} onChange={onChange} onBlur={onBlur} />
+                    {errors.length > 0 && <ul>
+                        {errors.map((error, index) => <li data-testid="error" key={index}>{error}</li>)}
+                    </ul>}
+                </>}
+            </Leaf>
+        </>
+    }
+
+    const { getAllByTestId } = render(<Wrapper />);
+
+    expect(getAllByTestId("error").map(node => node.textContent)).toEqual([
+        "Value is required",
+        "Value is required"
+    ]);
+});
--- src/Leaf.tsx
+++ src/Leaf.tsx
@@ -1,34 +1,52 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { get, set } from './domain'
 
 type Validator = (value: any) => Array<string> | any;
+type Update<TTarget> = (updatedModel: TTarget) => void;
+type Blur = () => void;
+type ValidationModel = {
+    set: React.Dispatch<any>,
+    get: (target: string) => Array<string>
+};
 
-export function useValidationFor(model: any) {
+export function useValidationFor(model: any): ValidationModel {
     const [validationModel, setValidationModel] = useState<any>({});
 
     return {
-        validate: (target: string) => ({
-            conformsTo: (validator: Validator) => {
-                setValidationModel((currentValidationModel: any) => ({
-                    ...currentValidationModel,
-                    [target]: validator(get(target).from(model))
-                }));
-            }
-        }),
-        validationResultsFor: (target: string) => validationModel[target] || []
+        set: setValidationModel,
+        get: (target: string) => validationModel[target] || []
     }
 }
 
 export function Leaf<TModel, TTarget>(props: {
-    children: (model: TTarget, onChange: (updatedModel: TTarget) => void) => any,
+    children: (model: TTarget, onChange: Update<TTarget>, onBlur: Blur, errors: Array<string>) => any,
     location: string,
     model: TModel,
-    onChange: React.Dispatch<React.SetStateAction<TModel>>
+    onChange: React.Dispatch<React.SetStateAction<TModel>>,
+    validationModel?: ValidationModel,
+    validators?: Array<Validator>,
+    showErrors?: boolean
 }) {
-    const { children, location, model, onChange } = props;
+    const [hasBlurred, setHasBlurred] = useState(false);
+    const { children, location, model, validationModel, validators, onChange, showErrors } = props;
+    const targetValue = get<any>(location).from(model);
+
+    useEffect(() => {
+        if (validationModel && validators && validators.length) {
+            validationModel.set((origValidationModel: any) => ({
+                ...origValidationModel,
+                [location]: validators
+                    .map(validator => validator(targetValue))
+                    .filter(value => value)
+                    .flat()
+            }));
+        }
+    }, [targetValue]);
 
     return children(
-        get<any>(location).from(model),
-        update => onChange(set(location).to(update).in(model))
+        targetValue,
+        update => onChange(set(location).to(update).in(model)),
+        () => setHasBlurred(true),
+        validationModel && (hasBlurred || showErrors) ? validationModel.get(location) : []
     );
 }
