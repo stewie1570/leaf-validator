ae68044bc311a1519faceb7f0a9fb2a228a67430
Stewart Anderson <stander@extendhealth.com>
2020-10-16 15:43:36 -0600

    Remove defer from useLoadingState hook.

--- src/lib/hooks/useLoadingState.test.tsx
+++ src/lib/hooks/useLoadingState.test.tsx
@@ -50,30 +50,9 @@ test("should show loading while resolving and not-loading once rejected", async 
     await findByText("the error");
 });
 
-test("should defer showing loading while resolving and then not-loading once resolved", async () => {
-    function TestComponent() {
-        const [isLoading, showLoadingWhile] = useLoadingState({ defer: 100 });
-        const [resolvedValue, setResolvedValue] = useState("not started");
-        const runTheQuery = async () => setResolvedValue(
-            await showLoadingWhile(wait(200).then(() => Promise.resolve("resolved value"))));
-
-        return <>
-            <button onClick={runTheQuery}>Execute</button>
-            {isLoading ? <div>Loading...</div> : <div>{resolvedValue}</div>}
-        </>;
-    }
-
-    const { getByText, findByText } = render(<TestComponent />);
-    getByText("not started");
-    getByText("Execute").click();
-    getByText("not started");
-    await findByText("Loading...");
-    await findByText("resolved value");
-});
-
 test("should show loading state for a minimum amount of time", async () => {
     function TestComponent() {
-        const [isLoading, showLoadingWhile] = useLoadingState({ defer: 100, minLoadingTime: 250 });
+        const [isLoading, showLoadingWhile] = useLoadingState({ minLoadingTime: 250 });
         const [resolvedValue, setResolvedValue] = useState("not started");
         const runTheQuery = async () => setResolvedValue(
             await showLoadingWhile(wait(200).then(() => Promise.resolve("resolved value"))));
@@ -87,33 +66,6 @@ test("should show loading state for a minimum amount of time", async () => {
     const { getByText, findByText } = render(<TestComponent />);
     getByText("not started");
     getByText("Execute").click();
-    getByText("not started");
     await findByText("Loading...");
     await findByText("resolved value");
-});
-
-test("should show not-loading (even when deferred) once rejected", async () => {
-    function TestComponent() {
-        const [isLoading, showLoadingWhile] = useLoadingState({ defer: 100 });
-        const [resolvedValue, setResolvedValue] = useState("not started");
-        const runTheQuery = async () => {
-            try {
-                setResolvedValue(await showLoadingWhile(wait(200).then(() => Promise.reject("the error"))));
-            }
-            catch (error) {
-                setResolvedValue(error);
-            }
-        }
-
-        return <>
-            <button onClick={runTheQuery}>Execute</button>
-            {isLoading ? <div>Loading...</div> : <div>{resolvedValue}</div>}
-        </>;
-    }
-
-    const { getByText, findByText } = render(<TestComponent />);
-    getByText("not started");
-    getByText("Execute").click();
-    await findByText("Loading...");
-    await findByText("the error");
 });
