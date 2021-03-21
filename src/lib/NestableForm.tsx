26841b2ef816b18cb8203e136e6eebc914a0eb9b
Stewart Anderson <stewie1570@gmail.com>
2021-03-20 23:41:57 -0600

    Initial for new approach: change the submit handler, not form location.

--- src/lib/NestableForm.tsx
+++ src/lib/NestableForm.tsx
@@ -1,10 +1,19 @@
-import React, { useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { createContext, useContext } from "react";
 import { useMountedOnlyState } from "./hooks/useMountedOnlyState";
 
 type CurrentFormId = string | undefined;
-type CurrentForm = [CurrentFormId, React.Dispatch<React.SetStateAction<CurrentFormId>>];
-type Form = { children: any, name: string, [otherPropNames: string]: any };
+type CurrentForm = [
+    CurrentFormId,
+    React.Dispatch<React.SetStateAction<CurrentFormId>>,
+    (name: string, handler: (event?: React.FormEvent<HTMLFormElement>) => void) => void
+];
+type Form = {
+    children: any,
+    name: string,
+    onSubmit: (event?: React.FormEvent<HTMLFormElement>) => void,
+    [otherPropNames: string]: any
+};
 
 const CurrentFormContext = createContext<CurrentForm | undefined>(undefined);
 const FormIdContext = createContext<any>(undefined);
@@ -22,25 +31,39 @@ export const inputWithFormSelectionOnFocus = (Input: React.ElementType) => (prop
     return <Input {...otherProps} onFocus={handleFocus} />;
 }
 
-export const formWithVirtualNestability = (Form: React.ElementType) => ({ children, name, ...otherProps }: Form) => {
-    const formInstanceRef = useRef(name);
+export const formWithVirtualNestability = (Form: React.ElementType) => ({ children, name, onSubmit, ...otherProps }: Form) => {
+    const [submitHandlers, setSubmitHandlers] = useState({
+        [name]: onSubmit
+    });
+    const addHandler = (name: string, handler: any) => {
+        setSubmitHandlers(submitHandlers => ({
+            ...submitHandlers,
+            [name]: handler
+        }));
+    }
     const currentFormContext = useContext(CurrentFormContext);
-    const [localForm, setLocalForm] = useMountedOnlyState<CurrentFormId>(formInstanceRef.current);
-    const [currentForm] = currentFormContext ?? [localForm, setLocalForm];
-    const isInRenderedForm = currentForm === formInstanceRef.current;
-    const isInOuterContext = Boolean(currentFormContext);
+    const [selectedHandlerName, , setHandler] = currentFormContext ?? [name, undefined, addHandler];
+    const [localFormName, setLocalFormName] = useMountedOnlyState<CurrentFormId>(name);
+    const isInsideForm = Boolean(currentFormContext);
 
-    const form = <FormIdContext.Provider value={formInstanceRef.current}>
-        {isInRenderedForm
-            ? <Form name={name} {...otherProps}>
-                {children}
-            </Form>
-            : children}
-    </FormIdContext.Provider>;
+    useEffect(() => {
+        isInsideForm && setHandler?.(name, onSubmit);
+    }, []);
 
-    return isInOuterContext
-        ? <>{form}</>
-        : <CurrentFormContext.Provider value={[localForm, setLocalForm]}>
-            {form}
-        </CurrentFormContext.Provider>;
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        localFormName && submitHandlers[localFormName]?.(event);
+    }
+
+    return <FormIdContext.Provider value={name}>
+        {
+            isInsideForm
+                ? children
+                : <CurrentFormContext.Provider value={[localFormName, setLocalFormName, addHandler]}>
+                    <Form {...otherProps} onSubmit={handleSubmit} name={name} >
+                        {children}
+                    </Form>
+                </CurrentFormContext.Provider>
+        }
+    </FormIdContext.Provider>
 };
