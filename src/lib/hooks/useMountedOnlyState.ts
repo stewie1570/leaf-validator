b93ab4342b35fb18c27865541dc9d73548c9696b
Stewart Anderson <stewart.anderson3@towerswatson.com>
2025-06-12 12:47:51 -0600

    Fixed a strict-mode bug

--- src/lib/hooks/useMountedOnlyState.ts
+++ src/lib/hooks/useMountedOnlyState.ts
@@ -3,8 +3,18 @@ import { useState, Dispatch, SetStateAction, useRef, useEffect } from "react"
 export const useMountedOnlyState = <T>(initialState: T): [T, Dispatch<SetStateAction<T>>] => {
     const [state, setState] = useState<T>(initialState);
     const instance = useRef({ isMounted: true });
+    instance.current.isMounted = true;
 
-    useEffect(() => () => { instance.current.isMounted = false; }, []);
+    useEffect(() => {
+        instance.current.isMounted = true;
 
-    return [state, updatedState => instance.current.isMounted && setState(updatedState)];
+        return () => {
+            instance.current.isMounted = false;
+        };
+    }, []);
+
+    return [state, updatedState => {
+        if (updatedState === 1) console.log({ mounted: instance.current.isMounted, state, updatedState });
+        instance.current.isMounted && setState(updatedState);
+    }];
 }
